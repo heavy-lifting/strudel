e975f5cab08f8ab2179980c83817f3dbbb95bb3e
heavy-lifting <lucy.cheesman@gmail.com>
2024-07-31 22:44:35 +0100

    amaze samples

--- website/src/config.ts
+++ website/src/config.ts
@@ -74,7 +74,8 @@ export const SIDEBAR: Sidebar = {
       { text: 'Recap', link: 'workshop/recap' },
       { text: 'Workshop in German', link: 'de/workshop/getting-started' },
       { text: 'Society of Explorers Workshop', link: 'workshop/soc-of-ex' },
-      { text: 'From-Scratch Workshop', link: 'workshop/from-scratch'}
+      { text: 'From-Scratch Workshop', link: 'workshop/from-scratch'},
+      { text: 'A MAZE./ Workshop', link: 'workshop/a-maze'}
     ],
     'Making Sound': [
       { text: 'Samples', link: 'learn/samples' },
